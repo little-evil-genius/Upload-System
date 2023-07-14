9a011af834a94f382703030dc1d2f0a242033f29
Lara <75126217+little-evil-genius@users.noreply.github.com>
2023-07-14 18:52:19 +0200

    Create uploadsystem.lang.php

---
+++ inc/languages/deutsch_du/admin/uploadsystem.lang.php
@@ -0,0 +1,119 @@
+<?php
+$l['uploadsystem_permission'] = "Kann das Uploadsystem verwalten?";
+$l['uploadsystem_manage'] = "Uploadsystem";
+$l['uploadsystem_manage_header'] = "Uploadsystem verwalten";
+
+$l['uploadsystem_manage_overview'] = "Upload-Elemente";
+$l['uploadsystem_manage_overview_desc'] = "In diesem Bereich kannst du die Upload-Elemente erstellen, verwalten und löschen.";
+$l['uploadsystem_manage_overview_name'] = "Name";
+$l['uploadsystem_manage_overview_path'] = "Upload-Pfad";
+$l['uploadsystem_manage_overview_extensions'] = "erlaubte	Dateitypen";
+$l['uploadsystem_manage_overview_dims'] = "Größe (Breite x Höhe)";
+$l['uploadsystem_manage_overview_dims_min'] = "<b>min.</b> {1} Pixel<br><b>max.</b> /";
+$l['uploadsystem_manage_overview_dims_minmax'] = "<b>min.</b> {1} Pixel<br><b>max.</b> {2} Pixel";
+$l['uploadsystem_manage_overview_dims_fix'] = "{1} Pixel";
+$l['uploadsystem_manage_overview_square'] = "Quadratisch";
+$l['uploadsystem_manage_overview_square_yes'] = "Ja";
+$l['uploadsystem_manage_overview_square_no'] = "Nein";
+$l['uploadsystem_manage_overview_size'] = "max. Dateigröße";
+$l['uploadsystem_manage_overview_options'] = "Optionen";
+$l['uploadsystem_manage_overview_options_edit'] = "Bearbeiten";
+$l['uploadsystem_manage_overview_options_delete'] = "Löschen";
+$l['uploadsystem_manage_no_elements'] = "Aktuell wurden keine Upload-Elemente erstellt.";
+$l['uploadsystem_manage_error_invalid'] = "Ungültige Option";
+
+$l['uploadsystem_manage_overview_delete_notice'] = "Soll dieses Upload-Elemente wirklich gelöscht werden? Es werden dann auch alle hochgeladene Datein für dieses Element gelöscht.";
+$l['uploadsystem_manage_overview_delete_logadmin'] = "Upload-Elemente gelöscht ";
+$l['uploadsystem_manage_overview_delete_flash'] = "Das Upload-Elemente wurde erfolgreich gelöscht.";
+
+$l['uploadsystem_manage_add_upload'] = "Upload-Element hinzufügen";
+$l['uploadsystem_manage_add_upload_desc'] = "Hier kannst du eine neue Upload-Elemente erstellen.";
+$l['uploadsystem_manage_add_upload_identification'] = "Identifikator <em>*</em>";
+$l['uploadsystem_manage_add_upload_identification_desc'] = "Dieser Titel dient als Name für den Speicher-Ordner und für die Variabel Bezeichnung im Template. Es sind keine Sonderzeichen oder Leerzeichen erlaubt (maschinenlesbar). Der Name muss eindeutig sein.";
+$l['uploadsystem_manage_add_upload_name'] = "Titel <em>*</em>";
+$l['uploadsystem_manage_add_upload_name_desc'] = "Wie lautet die Bezeichnung für das Upload-Elemente? Dieser wird auch später angezeigt.";
+$l['uploadsystem_manage_add_upload_description'] = "Kurzbeschreibung <em>*</em>";
+$l['uploadsystem_manage_add_upload_description_desc'] = "Beschreibe kurz dieses Element. Wo und wofür es beispielsweise verwendet wird.";
+$l['uploadsystem_manage_add_upload_disporder'] = "Sortierung <em>*</em>";
+$l['uploadsystem_manage_add_upload_disporder_desc'] = "Dies ist die Anzeigereihenfolge, abhängig von anderen Upload-Elemente. Die Zahl sollte für jedes Feld unterschiedlich sein.";
+$l['uploadsystem_manage_add_upload_extensions'] = "Erlaubte Dateitypen <em>*</em>";
+$l['uploadsystem_manage_add_upload_extensions_desc'] = "Welche Dateitypen sind für dieses Element erlaubt?";
+$l['uploadsystem_manage_add_upload_extensions_value'] = "{1} erlauben";
+$l['uploadsystem_manage_add_upload_mindims'] = "Minimale Grafik-Größe <em>*</em>";
+$l['uploadsystem_manage_add_upload_mindims_desc'] = "Die minimal zulässige Größe für dieses Element, Breite und Höhe getrennt durch x oder |.";
+$l['uploadsystem_manage_add_upload_maxdims'] = "Maximale Grafik-Größe";
+$l['uploadsystem_manage_add_upload_maxdims_desc'] = "Die maximal zulässige Größe für dieses Element, Breite und Höhe getrennt durch x oder |. Wenn das Feld leer bleibt, wird die Größe nicht beschränkt.";
+$l['uploadsystem_manage_add_upload_square'] = "Quadratisches Element <em>*</em>";
+$l['uploadsystem_manage_add_upload_square_desc'] = "Muss das Element quadratisch sein?";
+$l['uploadsystem_manage_add_upload_bytesize'] = "Maximale Datei-Größe <em>*</em>";
+$l['uploadsystem_manage_add_upload_bytesize_desc'] = "Die maximale Dateigröße (in Kilobyte) für hochgeladene Element (0 = Keine Beschränkung). Der Defaultwert beträgt 5 MB.";
+$l['uploadsystem_manage_add_upload_button'] = "Upload-Element speichern";
+$l['uploadsystem_manage_add_upload_error_identification'] = "Du hast keinen Identifikator für dieses Upload-Element angegeben.";
+$l['uploadsystem_manage_add_upload_error_name'] = "Du hast keinen Titel für dieses Upload-Element angegeben.";
+$l['uploadsystem_manage_add_upload_error_description'] = "Du hast keine Beschreibung für dieses Upload-Element angegeben.";
+$l['uploadsystem_manage_add_upload_error_extensions'] = "Du musst mindestens einen Dateityp für dieses Upload-Element erlauben.";
+$l['uploadsystem_manage_add_upload_error_mindims'] = "Du hast keine minimale Grafik-Größe für dieses Upload-Element angegeben.";
+$l['uploadsystem_manage_add_upload_logadmin'] = "Ein neues Upload-Element wurde hinzugefügt ";
+$l['uploadsystem_manage_add_upload_flash'] = "Das Upload-Element wurde erfolgreich erstellt.";
+
+$l['uploadsystem_manage_edit_element'] = "Upload-Element bearbeiten";
+$l['uploadsystem_manage_edit_element_desc'] = "Hier kannst du ein Upload-Element bearbeiten.";
+$l['uploadsystem_manage_edit_element_container'] = "Upload-Element: {1}";
+$l['uploadsystem_manage_edit_element_button'] = "Änderungen speichern";
+$l['uploadsystem_manage_edit_element_logadmin'] = "Upload-Element geändert ";
+$l['uploadsystem_manage_edit_element_flash'] = "Das Upload-Element wurde erfolgreich gespeichert.";
+
+$l['uploadsystem_manage_userfiles'] = "User-Dateien verwalten";
+$l['uploadsystem_manage_userfiles_desc'] = "In diesem Bereich kannst du die User-Datein verwalten und bearbeiten.";
+$l['uploadsystem_manage_userfiles_overview'] = "Übersicht aller User";
+$l['uploadsystem_manage_userfiles_options'] = "Optionen";
+$l['uploadsystem_manage_userfiles_options_edit'] = "Zeige hochgeladene Datein";
+$l['uploadsystem_manage_userfiles_options_delete'] = "Lösche alle hochgeladene Datein";
+$l['uploadsystem_manage_userfiles_no_users'] = "Es wurden keine User in der Datenbank gefunden. Überprüfe, ob du die Datenbank nicht aktualisieren musst.";
+
+$l['uploadsystem_manage_userfiles_delete_notice'] = "Soll alle hochgeladenen Datein von diesem Account wirklich gelöscht werden? Es werden alle Datein vom Server gelöscht und die Datenbank Zeile geleert.";
+$l['uploadsystem_manage_userfiles_delete_logadmin'] = "Alle Datein von {1} gelöscht";
+$l['uploadsystem_manage_userfiles_delete_flash'] = "Alle Datein von {1} wurden erfolgreich gelöscht";
+
+$l['uploadsystem_manage_usercheck'] = "Datenbank aktualisieren";
+$l['uploadsystem_manage_usercheck_desc'] = "Hier kannst du überprüfen, ob alle Accounts vom Forum auch in der Datenbank für die hochgeladenen Dateien vorhanden sind.";
+$l['uploadsystem_manage_usercheck_missing'] = "Fehlende Accounts";
+$l['uploadsystem_manage_usercheck_missing_desc'] = "Sollten Accounts in der Datenbank für die hochgeladenen Datein fehlen, können die fehlenden UIDs hier automatisch hinzugefügt werden.";
+$l['uploadsystem_manage_usercheck_option'] = "Option";
+$l['uploadsystem_manage_usercheck_option_full'] = "Alle Accounts sind in der Datenbank vorhanden.";
+$l['uploadsystem_manage_usercheck_logadmin'] = "Upload Datenbank aktualisiert ";
+$l['uploadsystem_manage_usercheck_flash'] = "Alle fehlenden Accounts wurden nachgetragen.";
+
+$l['uploadsystem_manage_edit_user'] = "Upload-Dateien bearbeiten";
+$l['uploadsystem_manage_edit_user_desc'] = "Hier kannst du die hochgeladenen Datei von diesem Benutzer ansehen, bearbeiten und auch löschen.";
+$l['uploadsystem_manage_edit_user_container'] = "Upload-Dateien von {1}";
+
+$l['uploadsystem_manage_edit_user_element_upload'] = "{1} hochladen:";
+$l['uploadsystem_manage_edit_user_element_dims_maxmin'] = "Die Datei kann eine Größe von {1} bis {2} Pixel besitzen.";
+$l['uploadsystem_manage_edit_user_element_dims_min'] = "Die <i>minimal</i> zulässige Größe beträgt {1} Pixel.";
+$l['uploadsystem_manage_edit_user_element_dims_one'] = "Die zulässige Größe beträgt {1} Pixel.";
+$l['uploadsystem_manage_edit_user_element_square'] = "Die Datei muss <b>quadratisch</b> sein.";
+$l['uploadsystem_manage_edit_user_element_extensions_plural'] = "Die zulässigen sind {1} Dateiformate.";
+$l['uploadsystem_manage_edit_user_element_extensions_singular'] = "Nur {1} Dateien sind zulässig.";
+$l['uploadsystem_manage_edit_user_element_size'] = "Die maximal zulässige Dateigröße beträgt {1}.";
+$l['uploadsystem_manage_edit_user_element_notice'] = "Wenn du ein anderes Bild wählst, wird das ältere Bild vom Server gelöscht.";
+
+$l['uploadsystem_manage_edit_user_element_button_add'] = "{1} hochladen";
+$l['uploadsystem_manage_edit_user_element_button_change'] = "{1} ändern";
+$l['uploadsystem_manage_edit_user_element_button_remove'] = "{1} löschen";
+
+$l['uploadsystem_manage_edit_user_error_upload'] = "Es wurde keine Datei zum hochladen ausgewählt, bitte wähle eine Datei aus um sie hochzuladen.";
+$l['uploadsystem_manage_edit_user_error_upload_dims_fixed'] = "Die Datei entspricht nicht den zugelassenen Maßen von {1}x{2} Pixel (Breite x Höhe).";
+$l['uploadsystem_manage_edit_user_error_upload_dims_square'] = "Die Datei ist nicht quadratisch. Bitte wähle eine quadratische Datei aus.";
+$l['uploadsystem_manage_edit_user_error_upload_dims_squareMini'] = "Die Datei ist zu klein und nicht quadratisch. Die minimal zulässige Größe beträgt {1}x{2} Pixel (Breite x Höhe).";
+$l['uploadsystem_manage_edit_user_error_upload_dims_squareMax'] = "Die Datei ist zu groß und nicht quadratisch. Die maximal zulässige Größe beträgt {1}x{2} Pixel (Breite x Höhe).";
+$l['uploadsystem_manage_edit_user_error_upload_dims_mini'] = "Die Datei ist zu klein. Die minimal zulässige Größe beträgt {1}x{2} Pixel (Breite x Höhe).";
+$l['uploadsystem_manage_edit_user_error_upload_dims_max'] = "Die Datei ist zu groß. Die maximal zulässige Größe beträgt {1}x{2} Pixel (Breite x Höhe).";
+$l['uploadsystem_manage_edit_user_error_upload_size'] = "Die Datei ist größer als {1}, bitte wähle eine kleinere Datei aus.";
+$l['uploadsystem_manage_edit_user_error_upload_file'] = "Die Datei besitzt ein ungültiges Dateiformat ({1}), bitte wähle eine andere Datei aus.";
+
+$l['uploadsystem_manage_edit_user_logadmin_remove'] = "Daten von {1} bearbeitet ";
+$l['uploadsystem_manage_edit_user_flash_remove'] = "Die Datei ({1}) wurde erfolgreich gelöscht.";
+$l['uploadsystem_manage_edit_user_logadmin_upload'] = "Daten von {1} bearbeitet ";
+$l['uploadsystem_manage_edit_user_flash_upload'] = "Die Datei ({1}) wurde erfolgreich hochgeladen.";
+?> 
