0b10920ae2128a6cf86dbdd6f5986e8d1c82bd2c
Lara <75126217+little-evil-genius@users.noreply.github.com>
2024-01-16 10:41:07 +0100

    Update uploadsystem.php

--- inc/plugins/uploadsystem.php
+++ inc/plugins/uploadsystem.php
@@ -35,7 +35,7 @@ function uploadsystem_info(){
 		"website"	=> "https://github.com/little-evil-genius",
 		"author"	=> "little.evil.genius",
 		"authorsite"	=> "https://storming-gates.de/member.php?action=profile&uid=1712",
-		"version"	=> "1.0",
+		"version"	=> "1.0.1",
 		"compatibility" => "18*"
 	);
 }
